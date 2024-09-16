e0c13648abcfc228b7a3491e99f53d36be7425cf
Dan <whatdanman@gmail.com>
2024-09-17 01:21:24 +0300

    play with rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,10 +3,44 @@ import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
+import postcss from "rollup-plugin-postcss";
+import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
+import clean from "@rollup-extras/plugin-clean";
 
-const bundle: RollupOptions = {
-  input: "src/main.ts",
-  output: { file: "dist/bundle.js" },
-  plugins: [typescript(), nodeResolve({ browser: true }), commonjs(), terser()],
-};
+const bundle = [
+  {
+    input: "src/main.ts",
+    output: { file: "dist/bundle.js" },
+    external: ["./index.html", "main.css"],
+    plugins: [
+      typescript(),
+      nodeResolve({ browser: true }),
+      commonjs(),
+      terser(),
+    ],
+  },
+  {
+    input: "main.css",
+    output: [{ file: "dist/main.css" }],
+    plugins: [
+      postcss({
+        minimize: true,
+      }),
+    ],
+  },
+  {
+    input: "index.html",
+    output: { dir: "dist" },
+    plugins: [
+      html(),
+      clean({
+        targets: [
+          "dist/bundle.js",
+          "dist/main.css",
+          "dist/rollup-plugin-html-noop.js",
+        ],
+      }),
+    ],
+  },
+];
 export default bundle;
